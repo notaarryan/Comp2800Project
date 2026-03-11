6fd9d112703336083988db2792d5e0508ece62d7
msimic1222 <markosimic1222@yahoo.com>
2026-03-10 21:26:56 -0400

    Implementation of JFrame and CanvasPanel

---
+++ src/whiteboardGUI.java
@@ -0,0 +1,29 @@
+/*******
+Marko Simic
+Whiteboard GUI Application
+3/10/25
+*******************/
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class whiteboardGUI { // class for the whiteboard app 
+
+    public static void main(String[] args) { // main method 
+ 
+        JFrame frame = new JFrame("Whiteboard GUI Application"); //JFrame created 
+        frame.setSize(800, 600); // window size set to 800x600
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close operation set to exit on close 
+        
+        CanvasPanel canvas = new CanvasPanel(); // new canvas panel object created to draw on
+        frame.add(canvas); // panel added to the frame
+
+        frame.setVisible(true); // frame set to visible so the window appears when the program runs
+    }
+}
+
+class CanvasPanel extends JPanel { // canvas panel class, JPanel extended to override the paintComponent method for drawing
+
+    public CanvasPanel() { // constructor for canvas panel 
+        setVisible(true); // ensures panel is visible when added to the frame 
+    }
+}
